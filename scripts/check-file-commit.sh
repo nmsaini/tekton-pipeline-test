b36aaa80001b5263af91272b9d4da81024d7ea51
nmsaini <nmsaini@gmail.com>
2022-09-29 14:15:34 -0700

    adding script to check file commited

---
+++ scripts/check-file-commit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+FILE2CHECK=temp-check-file
+if [ ! -z "$1" ]; then
+	FILE2CHECK=$1
+fi
+# list of changed files in the last commit
+filelist=$(echo $(git log -p | grep commit | head -2 | cut -f2 -d' ') | while read s e; do git diff --name-only $s..$e; done)
+if [[ $filelist == *"${FILE2CHECK}"* ]]; then
+    echo "Got file interested in!"
+else
+    echo "File $FILE2CHECK not found!"
+    exit 99
+fi
