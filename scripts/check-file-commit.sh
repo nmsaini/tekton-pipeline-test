e74003cd026037a35c59130e188fefd55a66bf3f
nmsaini <nmsaini@gmail.com>
2022-09-30 07:29:07 -0700

    changes

--- scripts/check-file-commit.sh
+++ scripts/check-file-commit.sh
@@ -4,7 +4,8 @@ if [ ! -z "$1" ]; then
 	FILE2CHECK=$1
 fi
 # list of changed files in the last commit
-filelist=$(echo $(git log -p | grep commit | head -2 | cut -f2 -d' ') | while read s e; do git diff --name-only $s..$e; done)
+filelist=$(echo $(git log -p | grep "^commit" | head -2 | cut -f2 -d' ') | while read s e; do git diff --name-only $s..$e; done)
+echo "$filelist"
 if [[ $filelist == *"${FILE2CHECK}"* ]]; then
     echo "Got file interested in!"
 else
