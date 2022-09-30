824cc2a703b976d8ff39cc6c98d69c0fe5cf50da
nmsaini <nmsaini@gmail.com>
2022-09-30 07:31:49 -0700

    updated

--- scripts/check-file-commit.sh
+++ scripts/check-file-commit.sh
@@ -7,7 +7,7 @@ fi
 filelist=$(echo $(git log -p | grep "^commit" | head -2 | cut -f2 -d' ') | while read s e; do git diff --name-only $s..$e; done)
 echo "$filelist"
 if [[ $filelist == *"${FILE2CHECK}"* ]]; then
-    echo "Got file interested in!"
+    echo "Got file $FILE2CHECK in commited list! Continuing pipeline..."
 else
     echo "==== File $FILE2CHECK not in commited list! ===="
     echo "==== Exiting with ERR 99 ===="
